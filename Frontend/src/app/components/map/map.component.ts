eb8cdc7860f15d605317eaea089d2b6bf39c31c3
NMishchenko <mishchenko.nls@gmail.com>
2023-04-08 18:01:18 +0300

    Add working map with test marker

--- Frontend/src/app/components/map/map.component.ts
+++ Frontend/src/app/components/map/map.component.ts
@@ -9,18 +9,33 @@ import * as mapboxgl from 'mapbox-gl';
 export class MapComponent implements OnInit {
   map: mapboxgl.Map;
   style = 'mapbox://styles/mapbox/streets-v11';
-  lat = 37.75;
-  lng = -122.41;
-  constructor() { }
+  lat = 26.3398;
+  lng = -81.7787;
+
+  constructor() {}
+
   ngOnInit() {
-    (mapboxgl as any).accessToken = 'pk.eyJ1Ijoib2F0YW1hbmNodWsiLCJhIjoiY2xnN3k1ODU2MHM3NjNnbzkzMnJ5amFrOSJ9.fUOEKrtONXbZfxQsn4YDlw';
-      this.map = new mapboxgl.Map({
-        container: 'map',
-        style: this.style,
-        zoom: 13,
-        center: [this.lng, this.lat]
+    mapboxgl as typeof mapboxgl;
+    this.map = new mapboxgl.Map({
+      accessToken:
+        'pk.eyJ1Ijoib2F0YW1hbmNodWsiLCJhIjoiY2xnN3k1ODU2MHM3NjNnbzkzMnJ5amFrOSJ9.fUOEKrtONXbZfxQsn4YDlw',
+      container: 'map',
+      style: this.style,
+      zoom: 2,
+      center: [this.lng, this.lat],
     });
-    // Add map controls
+
     this.map.addControl(new mapboxgl.NavigationControl());
+
+    const customMarker = document.createElement('div');
+    customMarker.style.backgroundImage = 'url(https://www.graphicpie.com/wp-content/uploads/2020/11/red-among-us-png.png)';
+    customMarker.style.backgroundSize = 'cover';
+    customMarker.style.backgroundPosition = 'center';
+    customMarker.style.width = '27px';
+    customMarker.style.height = '41px';
+
+    new mapboxgl.Marker(customMarker)
+      .setLngLat([30.5, 50.5])
+      .addTo(this.map);
   }
 }
