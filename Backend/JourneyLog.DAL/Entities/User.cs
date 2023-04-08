eaafed60a1c4fc16bdbd4b1f9ae575356905096f
demakk <dimademch03@gmail.com>
2023-04-08 18:57:26 +0300

    Adding all basic entities

---
+++ Backend/JourneyLog.DAL/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace JourneyLog.DAL.Entities;
+
+public class User : BaseEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    public ICollection<TravelLog> TravelLogs { get; set; }
+    public ICollection<UserPlace> UserPlaces { get; set; }
+}
